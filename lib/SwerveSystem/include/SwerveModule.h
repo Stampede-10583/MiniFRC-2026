405348bb9179624be845af5c8f5241266cddc8af
Lup1n5 <aidan.korten@providenceday.org>
2026-04-02 19:07:25 -0400

    started working on swerve drive

--- lib/SwerveSystem/include/SwerveModule.h
+++ lib/SwerveSystem/include/SwerveModule.h
@@ -1,25 +1,94 @@
-#ifndef  Morse_h
-#define Morse_h
+#ifndef SwerveModule_h
+#define SwerveModule_h
 #include "Arduino.h"
 #include "Alfredo_NoU3.h"
 #include "Alfredo_NoU3_encoder.h"
-class SwerveModule {
-    public:
-        SwerveModule(NoU_Motor* drive, NoU_Servo* turn, Encoder* driveencoder, float angleoffset, float wrapendpoint,  float drivegearratio, float turngearratio);
-        void driveModule(float targetAngle, float driveSpeed);
-        void directDriveModule(float targetAngle, float driveSpeed);
-        float getModuleAngle();
-        float getModuleVelocity();
-        SwerveModule getModule();
-    private: 
-        NoU_Motor *driveMotor;
-        NoU_Servo *turnMotor;
-        Encoder *driveEncoder;
-        float angleOffset;
-        float wrapEndpoint;
-        float driveGearRatio;
-        float turnGearRatio;
-        float currentSpeed;
-        float currentAngle;
+class SwerveModule
+{
+public:
+    /**
+     * Construct a swerve module controller.
+     *
+     * @param driveServoPort Port for the drive motor controller.
+     * @param driveMotorInversion True to invert the drive motor direction.
+     * @param turnServoPort Port for the steering servo.
+     * @param turnServoInversion True to invert steering direction.
+     * @param angleoffset Steering zero offset in degrees.
+     * @param wrapendpoint Steering wrap limit in degrees.
+     * @param drivegearratio Drive gear ratio from motor to wheel.
+     * @param turngearratio Steering gear ratio from servo to module.
+     * @param brakeMode True to enable drive motor braking.
+     */
+    SwerveModule(uint8_t driveServoPort, bool driveMotorInversion = false, uint8_t turnServoPort, bool turnServoInversion = false, float angleoffset, float wrapendpoint, float drivegearratio, float turngearratio, bool brakeMode = false);
+
+    /**
+     * Drive the module using a field-relative target angle.
+     *
+     * @param targetAngle Desired field-relative steering angle in degrees.
+     * @param driveSpeed Desired non-directional drive speed command.
+     */
+    void driveModule(float targetAngle, float driveSpeed);
+
+    /**
+     * Drive the module using a robot-relative target angle.
+     *
+     * @param targetAngle Desired robot-relative steering angle in degrees.
+     * @param driveSpeed Desired directional drive speed command.
+     */
+    void directDriveModule(float targetAngle, float driveSpeed);
+
+    /**
+     * Get the current robot-relative module angle.
+     *
+     * @return Current module steering angle in degrees.
+     */
+    float getModuleAngle();
+
+    /**
+     * Get the current module drive velocity.
+     *
+     * @return Current wheel speed estimate.
+     */
+    float getModuleVelocity();
+
+    /**
+     * Set brake mode for the drive motor.
+     *
+     * @param brake True to enable braking, false to coast.
+     */
+    void setBrakeMode(bool brake);
+
+    /**
+     * Get direct access to the drive motor object.
+     *
+     * @return Pointer to the internal drive motor (never nullptr).
+     */
+    NoU_Motor *getDriveMotor();
+
+    /**
+     * Get direct access to the steering servo object.
+     *
+     * @return Pointer to the internal steering servo (never nullptr).
+     */
+    NoU_Servo *getTurnServo();
+
+    /**
+     * Get a copy of this module object.
+     *
+     * @return Copy of the current SwerveModule instance.
+     */
+    SwerveModule getModule();
+
+private:
+    NoU_Motor driveMotor;
+    NoU_Servo turnServo;
+    Encoder driveEncoder;
+    float angleOffset;
+    float wrapEndpoint;
+    float driveGearRatio;
+    float turnGearRatio;
+    float currentSpeed;
+    float currentAngle;
+    bool turnInversion;
 };
 #endif
--- lib/SwerveSystem/src/SwerveModule.cpp
+++ lib/SwerveSystem/src/SwerveModule.cpp
@@ -2,55 +2,58 @@
 #include "SwerveModule.h"
 #include "Alfredo_NoU3.h"
 #include "Alfredo_NoU3_encoder.h"
-/*Constructor: takes in a NoU_Motor for the drive, a NoU_Servo for the turn, and Encoder for the drive encoder.
-    @param angleoffset the angle of the servo at zero position in degrees
-    @param wrapendpoint the angle where the servo cant move anymore in degrees
-    @param drive/turngearratio gear ratio of motor to mechanism
-*/
-SwerveModule::SwerveModule(NoU_Motor *drive, NoU_Servo *turn, Encoder *driveencoder, float angleoffset, float wrapendpoint, float drivegearratio, float turngearratio)
-    : driveMotor(drive), turnMotor(turn), driveEncoder(driveencoder)
+SwerveModule::SwerveModule(uint8_t driveMotorPort, bool driveMotorInversion, uint8_t turnServoPort, bool turnServoInversion, float angleoffset, float wrapendpoint, float drivegearratio, float turngearratio, bool brakeMode)
+    : driveMotor(NoU_Motor(driveMotorPort)), turnServo(NoU_Servo(turnServoPort))
 {
     angleOffset = angleoffset;
     wrapEndpoint = wrapendpoint;
     driveGearRatio = drivegearratio;
     turnGearRatio = turngearratio;
-    driveMotor->beginEncoder();
+    turnInversion = turnServoInversion;
+    driveMotor.setInverted(driveMotorInversion);
+    driveMotor.setBrakeMode(brakeMode);
+    driveMotor.beginEncoder();
 };
-/* Move the module field oriented. The speed is scalar, and the angle will be adjusted internally to account for the offset and deadspot etc
-    @param targetAngle the desired field-oriented angle in degrees
-    @param driveSpeed the desired NON DIRECTIONAL speed in m/s
-*/
-void SwerveModule::driveModule(float targetAngle, float driveSpeed) {
-    bool driveInversion =false;
+void SwerveModule::driveModule(float targetAngle, float driveSpeed)
+{
+    bool driveInversion = false;
     float trueAngle = targetAngle - angleOffset; // make it field relative
-    
-    driveMotor->set(driveInversion? driveSpeed : -driveSpeed);// do things and crap but field oriented!
+
+    driveMotor.set(driveInversion ? driveSpeed : -driveSpeed); // do things and crap but field oriented!
 };
 
-/* Move the module explicitly. The angle will be adjusted internally to account for the offset
-    @param targetAngle the desired ROBOT ORIENTED angle in degrees
-    @param driveVelocity the desired DIRECTIONAL VELOCITY in m/s
-*/
-void SwerveModule::directDriveModule(float targetAngle, float driveVelocity) {
+void SwerveModule::directDriveModule(float targetAngle, float driveVelocity)
+{
     // do things not field oriented
     float trueAngle = targetAngle - angleOffset;
-    driveMotor->set(driveVelocity);
+    driveMotor.set(driveVelocity);
 };
 
-/* Returns the current angle robot oriented of the module in degrees.
- */
 float SwerveModule::getModuleAngle()
 {
     return currentAngle - angleOffset;
 };
-/* Returns the current running velocity of the drive motor in m/s
- */
+
 float SwerveModule::getModuleVelocity()
 {
     return currentSpeed;
 };
-/*Returns a pointer to the module
- */
+
+void SwerveModule::setBrakeMode(bool brake)
+{
+    driveMotor.setBrakeMode(brake);
+};
+
+NoU_Motor *SwerveModule::getDriveMotor()
+{
+    return &driveMotor;
+};
+
+NoU_Servo *SwerveModule::getTurnServo()
+{
+    return &turnServo;
+};
+
 SwerveModule SwerveModule::getModule()
 {
     return *this;
